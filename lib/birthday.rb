8bf6291d9de48be652998863a1caf2331defcc2a
Celal Cankaya <celalcankaya@outlook.com>
2019-07-11 13:21:03 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,8 +6,6 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if age.to_i < 13
-      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    end
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
